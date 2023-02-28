fb27bc3657092c8d00342c77c5d85b6d6e6c637e
Waleed1123 <waleed007896@gmail.com>
2023-02-28 15:12:31 +0500

    styling Remaining

---
+++ Calculator.js
@@ -0,0 +1,70 @@
+
+
+let percentage = prompt("Enter Your Percentage")
+
+
+if(percentage>100){
+    alert('invailed Percentage');
+    console.log('invailed Percentage');
+
+}
+
+else if(percentage >=80)
+{   alert('Your Grade is A1')
+console.log('Your Grade is A1')
+    
+}
+
+else if(percentage >= 70) 
+{
+    alert('Your Grade is A')
+    console.log("Your Grade is A")
+}
+
+else if(percentage>= 60) {
+    alert ('Your Grade is B')
+    console.log("Your Grade is B")
+
+}
+
+else if (percentage>= 50)
+{
+    alert ('Your Grade is C')
+    console.log("Your Grade is C")
+
+}
+
+else if (
+    percentage>= 40
+)
+{
+    alert("YourGrade is D")
+    console.log ("Your Grade is D")
+}
+
+else if (percentage>= 33)
+{
+    alert("Your Grade is E")
+    console.log ("Your Grade is E")
+
+}
+
+else if (percentage>= 0)
+{
+    alert("Your are Fail")
+    console.log("You Are Fail")
+}
+else 
+{
+    console.log ("Invalid Percentage")
+    alert ("Invalid Percentage")
+}
+
+
+
+
+
+
+
+
+
